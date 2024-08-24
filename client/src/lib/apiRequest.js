36e2840375290eee027448f5c2bffcaea0173e10
Sumanth Poojary <128285327+sumanth639@users.noreply.github.com>
2024-08-24 20:16:11 +0530

    Update apiRequest.js

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "https://xestate-backend.onrender.com",
+  baseURL: "https://xestate-backend.onrender.com/api",
   withCredentials: true,
 });
 
