f7208ae7ca31e75e5533b80ebd1629d3e71a6e69
Sumanth Poojary <128285327+sumanth639@users.noreply.github.com>
2024-08-25 12:27:20 +0530

    Update app.js

--- api/app.js
+++ api/app.js
@@ -9,8 +9,11 @@ import chatRoute from "./routes/chat.route.js";
 import messageRoute from "./routes/message.route.js";
 
 const app = express();
-
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(cors({
+  origin: "https://xestate-client.onrender.com",
+  credentials: true
+}));
+//app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
